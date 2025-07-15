61d479db7e20618971ac4498071d61739f2e2797
Denis Gómez <denis.gomez.pineda@gmail.com>
2025-07-15 09:50:22 -0600

    Agregar entidad Catedratico

---
+++ src/models/Catedratico.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, Unique, OneToOne, JoinColumn, Index, BaseEntity } from "typeorm";
+import { Usuario } from "./Usuario";
+
+@Entity("catedratico")
+export class Catedratico extends BaseEntity {
+    @PrimaryColumn({ name:"dpi", type: "nvarchar" })
+    dpi: string;
+
+    @Column({ name:"id_usuario", type: "nvarchar", length: 50 })
+    @Index("IDX_UNIQUE_ID_USUARIO", {unique: true})
+    idUsuario: string;
+
+    @OneToOne(() => Usuario, usuario => usuario.usuario)
+    @JoinColumn()
+    usuario:Usuario;
+
+    @CreateDateColumn({ name:"created_at", type: "timestamp" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name:"updated_at", type: "timestamp" })
+    updatedAt: Date;
+
+    @DeleteDateColumn({ name:"deleted_at", type: "timestamp", nullable: true })
+    deletedAt?: Date | null;
+}
