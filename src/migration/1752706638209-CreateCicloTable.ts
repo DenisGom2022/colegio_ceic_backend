3de758dfe68bbe9c3c792e182a60306cc20d6c2d
Denis Gómez <denis.gomez.pineda@gmail.com>
2025-07-16 16:58:21 -0600

    corregir migration

---
+++ src/migration/1752706638209-CreateCicloTable.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCicloTable1752706638209 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+            await queryRunner.createTable(
+                new Table({
+                    name: "ciclo",
+                    columns: [
+                        {
+                            name: "id",
+                            type: "int",
+                            isPrimary: true,
+                            isNullable: false,
+                        },
+                        {
+                            name: "descripcion",
+                            type: "varchar",
+                            length: "255",
+                            isNullable: false,
+                        },
+                        {
+                            name: "created_at",
+                            type: "datetime",
+                            default: "CURRENT_TIMESTAMP",
+                        },
+                        {
+                            name: "updated_at",
+                            type: "datetime",
+                            default: "CURRENT_TIMESTAMP",
+                            onUpdate: "CURRENT_TIMESTAMP",
+                        },
+                        {
+                            name: "deleted_at",
+                            type: "datetime",
+                            isNullable: true,
+                        },
+                    ],
+                })
+            );
+        }
+    
+        public async down(queryRunner: QueryRunner): Promise<void> {
+            await queryRunner.dropTable("ciclo");
+        }
+
+}
